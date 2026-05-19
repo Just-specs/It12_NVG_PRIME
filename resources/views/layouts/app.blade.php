2604cdab9cc93ef3587e98b13bb4706f418271c0
Just-specs <j.sayson.546917@umindanao.edu.ph>
2026-05-19 14:48:11 +0800

    add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dispatch System')</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/NVG_LOGO_org.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/NVG_LOGO_org.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
